fb4bde65b42869a076d743a4c26098e40c250925
Sankar16 <31141239+Sankar16@users.noreply.github.com>
2017-08-30 17:03:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,16 @@
 console.log('Loaded!');
-
+//Change text
 var element= document.getElementById('main-text');
 element.innerHTML='Yo Logang';
+
+//move image
+var img=document.getElementById('Img1');
+var marginLeft=0;
+function moveRight(){
+  marginLeft=marginLeft+10;  
+  img.style.marginLeft=marginLeft+'px';
+}
+img.onclick=function(){
+    var interval=setInterval(moveRight,100);
+
+};
